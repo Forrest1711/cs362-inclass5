1cdd842ab4d9114a7e93fb71d91b3543d9276182
Forrest <forrest.90h@gmail.com>
2021-05-22 23:36:11 -0700

    more minor changes

--- test_leapYear.py
+++ test_leapYear.py
@@ -4,16 +4,16 @@ import unittest
 
 class TestCase(unittest.TestCase):
     def test1(self):
-        self.assertEqual(leap_year.leap_year(4), True)
+        self.assertTrue(leap_year.leap_year(4))
 
     def test2(self):
-        self.assertEqual(leap_year.leap_year(137), False)
+        self.assertFalse(leap_year.leap_year(137))
 
     def test3(self):
-        self.assertEqual(leap_year.leap_year(-32), True)
+        self.assertTrue(leap_year.leap_year(-32))
 
     def test4(self):
-        self.assertEqual(leap_year.leap_year("32"), True)
+        self.assertTrue(leap_year.leap_year("32"))
 
 
 if __name__ == '__main__':
