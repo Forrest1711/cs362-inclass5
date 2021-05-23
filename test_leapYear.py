05f84a5fa964779744a939658b8d7cc4f10499f7
Forrest <forrest.90h@gmail.com>
2021-05-22 23:17:28 -0700

    minor change

--- test_leapYear.py
+++ test_leapYear.py
@@ -10,7 +10,7 @@ class TestCase(unittest.TestCase):
         self.assertEqual(leap_year.leap_year(137), False)
 
     def test3(self):
-        self.assertEqual(leap_year.leap_year(-32), False)
+        self.assertEqual(leap_year.leap_year(-32), True)
 
     def test4(self):
         self.assertEqual(leap_year.leap_year("32"), False)
